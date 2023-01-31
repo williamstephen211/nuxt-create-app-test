1faa100d3ed6bb3d4328f90a29f13eef299456f0
node_orm_sequelize <williamstephen211@gmail.com>
2023-01-31 16:16:43 +0900

    first

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,10 @@ export default {
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
   ],
-
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
